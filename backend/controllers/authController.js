e4ac25d244accf5e045a941189efc01d25b004b8
Mia Flores <flores.miaet36@gmail.com>
2026-09-06 19:31:23 -0300

    configuracion controllers

---
+++ backend/controllers/authController.js
@@ -0,0 +1,31 @@
+const authService = require("../services/authService");
+const AuthService = require("../services/authService");
+
+class authController{
+
+    async registrar(req, res){
+        try{
+            const nuevoUsuario = await authService.registrar(req.body);
+
+            res.status(201).json({mensaje: "Usuario registrado correctamente",
+                usuario: {
+                    id_usuario: nuevoUsuario.id_usuario,
+                    nombre: nuevoUsuario.nombre,
+                    apellido: nuevoUsuario.apellido,
+                    username: nuevoUsuario.username,
+                    mail: nuevoUsuario.mail,
+                    rol: nuevoUsuario.rol
+                }
+            });
+
+        }
+
+        catch (error) {
+            res.status(400).json({
+                mensaje: error.mensaje
+            });
+        }
+    }
+}
+
+module.exports = new authController();
