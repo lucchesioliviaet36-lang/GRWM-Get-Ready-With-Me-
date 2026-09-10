991fe8c1cb1a442102c34ea4dee361631af666cf
Mia Flores <flores.miaet36@gmail.com>
2026-09-09 21:51:46 -0300

    controlers AI

---
+++ backend/controllers/catalogoController.js
@@ -0,0 +1,24 @@
+import catalogoService from "../services/catalogoService";
+
+const obtenerPorCategoria = async (req, res) => {
+    try{
+        const {categoria} = req.params;
+        const productos = await catalogoService.obtenerPorCategoria(categoria);
+
+        return res.status(200).json({
+            success: true,
+            productos
+        });
+
+    }
+    catch (error) {
+        console.error("Error obtenido del catalogo: ", error);
+
+        return res.status(500).json({
+            success: false,
+            message: "No se pudo obtener el catalogo"
+        });
+    }
+};
+
+export default {obtenerPorCategoria};
